f843602fadee6108c5cf752dd0ef40a0906246c2
MrTango <md@derico.de>
2014-12-08 16:26:47 +0100

    fix name of readme file in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="A folder view, which select the first page in a folder"\
     "which the user can access.",
     long_description=
-        open("README.txt").read() + "\n" +
+        open("README.rst").read() + "\n" +
         open(os.path.join("docs", "HISTORY.txt")).read(),
     # Get more strings from
     # http://pypi.python.org/pypi?:action=list_classifiers
